9f1c1416ed2c4df52fb13beb02a7c0937236891e
cjeffreyRC <chris.jeffrey@robertsoncollege.net>
2015-12-08 16:48:55 -0600

    Added DAL, Created Class Library,  Fixed References, Created User Class and Security Class.

---
+++ LoveDev/LoveDevLib/Security.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Data;
+using DAL_Project;
+
+namespace LoveDevLib
+{
+    static public class Security
+    {
+        static public User CurrentUser
+        {
+            get
+            {
+                User user = (User)HttpContext.Current.Session["User"];
+                if (HttpContext.Current.Session["User"] != null)
+                {
+                    user = getUserByID(user.UserID);
+                    HttpContext.Current.Session["User"] = user;
+                }
+                return user;
+            }
+        }
+        static public bool Login(string username, string password)
+        {
+            User user = getUser(username, password);
+            if (user != null)
+            {
+                HttpContext.Current.Session["user"] = user;
+                return true;
+            }
+            else
+            {
+                DAL d = new DAL();
+                d.AddParam("username", username);
+                d.AddParam("password", password);
+                d.ExecuteNonQuery("spAddFailedLogin");
+                return false;
+            }
+        }
+
+        static public void RegisterUser(string FirstName, string LastName, int Age, string City, string Country, string Email, int GenderID, int SexualOrientationID, bool IsActive)
+        {
+            DAL d = new DAL();
+            d.AddParam("FirstName", FirstName);
+            d.AddParam("LastName", LastName);
+            d.AddParam("Age", Age);
+            d.AddParam("City", City);
+            d.AddParam("Country", Country);
+            d.AddParam("Email", Email);
+            d.AddParam("GenderID", GenderID);
+            d.AddParam("SexualOrientation", SexualOrientationID);
+            d.AddParam("IsActive", IsActive);
+            d.ExecuteNonQuery("spRegisterNewUser");
+        }
+
+        static private User getUser(string Email, string Password)
+        {
+            DAL d = new DAL();
+            d.AddParam("Email", Email);
+            d.AddParam("Password", Password);
+            DataSet ds = d.ExecuteProcedure("spLogin");
+            return UserFactory(ds);
+        }
+        static private User getUserByID(int userId)
+        {
+            DAL d = new DAL();
+            d.AddParam("userID", userId);
+            DataSet ds = d.ExecuteProcedure("spGetUserById");
+            return UserFactory(ds);
+        }
+
+        static private User UserFactory(DataSet ds)
+        {
+            DataRow DR = ds.Tables[0].Rows[0];
+            return new User(DR["FirstName"].ToString(), DR["LastName"].ToString(), int.Parse(DR["Age"].ToString()), DR["City"].ToString(), DR["Country"].ToString(), DR["Email"].ToString(), int.Parse(DR["GenderID"].ToString()), int.Parse(DR["SexualOrientationID"].ToString()), (bool)DR["IsAdmin"], (bool)DR["IsActive"]);
+        }
+    }
+}
---
+++ LoveDev/LoveDevLib/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoveDevLib
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public string UserPhoto { get; set; }
+        public int GenderID { get; set; }     
+        public int SexualOrientationID { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsActive { get; set; }
+
+        public User(string FirstName, string LastName, int Age, string City, string Country, string Email, int GenderID, int SexualOrientationID, bool IsAdmin, bool IsActive)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Age = Age;
+            this.City = City;
+            this.Country = Country;
+            this.Email = Email;
+            this.IsActive = IsActive;
+            this.IsAdmin = IsAdmin;
+            this.GenderID = GenderID;
+            this.SexualOrientationID = SexualOrientationID;
+        }
+    }
+}
