9993d6a7408f66f5fd34fe0cf959ccf69c737135
TarantulaKeeper <t.j.petrowski@robertsoncollege.net>
2015-12-08 15:14:52 -0600

    Added a SQL file and began to create tables

---
+++ LoveDev/LoveDev/INSTALL.sql
@@ -0,0 +1,43 @@
+﻿USE MASTER;
+GO
+DROP DATABASE dbLoveDev;
+GO
+CREATE DATABASE dbLoveDev;
+GO
+USE dbLoveDev;
+
+-- TABLE FOR GENDERS
+
+CREATE TABLE tbGender(
+GenderID INT PRIMARY KEY IDENTITY (1,1),
+GenderName VARCHAR(50)
+)
+
+INSERT INTO tbGender(GenderName) VALUES ('Man'), ('Woman'), ('BiGender'), ('GenderQueer'), ('Non-Binary'),
+	('Two Spirit'), ('Transgender'), ('Transsexual')
+
+-- TABLE FOR SEXUAL ORIENTATION 
+
+CREATE TABLE tbSexualOrientation(
+SexualOrientationID INT PRIMARY KEY IDENTITY (1,1),
+SexualOrientationName VARCHAR(50)
+)
+
+INSERT INTO tbSexualOrientation(SexualOrientationName) VALUES ('Straight'), ('Gay'), ('Lesbian'), ('Asexual'),
+	('Pansexual'), ('Bisexual'), ('Sapiosexual'), ('Heteroflexible'), ('Homoflexible')
+
+-- TABLE FOR USERS
+
+CREATE TABLE tbUsers(
+UserID INT PRIMARY KEY IDENTITY (1,1),
+FirstName VARCHAR(50),
+LastName VARCHAR(50),
+Age INT,
+City VARCHAR(100),
+Country VARCHAR(50),
+Email VARCHAR(50),
+IsActive BIT,
+GenderID INT FOREIGN KEY REFERENCES tbGender(GenderID),
+SexualOrientationID INT FOREIGN KEY REFERENCES tbSexualOrientation(SexualOrientationID)
+)
+
