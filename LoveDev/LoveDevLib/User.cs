353e1744a6dabc202c179897ecd6e84c2b33d295
cjeffreyRC <chris.jeffrey@robertsoncollege.net>
2015-12-14 15:26:53 -0600

    Added spLogin Added UserID to constructor in user class

--- LoveDev/LoveDevLib/User.cs
+++ LoveDev/LoveDevLib/User.cs
@@ -21,7 +21,7 @@ namespace LoveDevLib
         public bool IsAdmin { get; set; }
         public bool IsActive { get; set; }
 
-        public User(int userID, string FirstName, string LastName, int Age, string City, string Country, string Email, int GenderID, int SexualOrientationID, bool IsAdmin, bool IsActive)
+        public User(int UserID, string FirstName, string LastName, int Age, string City, string Country, string Email, int GenderID, int SexualOrientationID, bool IsAdmin, bool IsActive)
         {
             this.UserID = UserID;
             this.FirstName = FirstName;
