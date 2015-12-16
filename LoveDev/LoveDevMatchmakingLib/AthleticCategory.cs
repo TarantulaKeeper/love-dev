0cc4c673aded1aa94089e9e5020f714ca7db6596
nPastulovic <pastulovic@hotmail.ca>
2015-12-16 16:07:24 -0600

    Added Class Library for Matchmaking, added class structure for the questions.

---
+++ LoveDev/LoveDevMatchmakingLib/AthleticCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoveDevMatchmakingLib
+{
+    class AthleticCategory:Questions
+    {
+        public int TotalValue { get; set; }
+        public List<string> Questions { get; set; }
+    }
+}
