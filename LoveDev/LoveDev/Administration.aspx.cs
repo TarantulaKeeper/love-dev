2acb253fe9338ee294006ede86c83fa1e94348e0
cjeffreyRC <chris.jeffrey@robertsoncollege.net>
2015-12-17 16:31:03 -0600

    Added security to pages (Must be logged in) Must be admin to access admin page

--- LoveDev/LoveDev/Administration.aspx.cs
+++ LoveDev/LoveDev/Administration.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using LoveDevLib;
 
 namespace LoveDev
 {
@@ -11,7 +12,10 @@ namespace LoveDev
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Security.CurrentUser == null || !Security.CurrentUser.IsAdmin)
+            {
+                Response.Redirect("Index.aspx?Must be logged in to view that page");
+            }
         }
     }
 }
--- LoveDev/LoveDev/Home.aspx.cs
+++ LoveDev/LoveDev/Home.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using LoveDevLib;
 
 namespace LoveDev
 {
@@ -11,7 +12,13 @@ namespace LoveDev
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                if (Security.CurrentUser == null)
+                {
+                    Response.Redirect("Index.aspx?Must be logged in to view that page");
+                }
+            }
         }
     }
 }
