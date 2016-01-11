693ac2696167df722be8ef34e6770156629b8239
TarantulaKeeper <t.j.petrowski@robertsoncollege.net>
2016-01-11 15:19:39 -0600

    Changed User table back

--- LoveDev/LoveDev/INSTALL.sql
+++ LoveDev/LoveDev/INSTALL.sql
@@ -39,7 +39,7 @@ Bio VARCHAR(500) NOT NULL
 INSERT INTO tbUser(FirstName, LastName, Password, Age, City, Country, Email, IsActive, IsAdmin, UserPhoto, GenderID, Bio)
 	VALUES 
 		('Niko', 'Pastulovic', '1234', 20, 'Winnipeg', 'Canada', 'niko.pastulovic@robertsoncollege.net', 1, 1, 'Images/NoPhoto.jpg', 1,''),
-		('T.J.', 'Petrowski', '1234', 24, 'Warren', 'Canada', 'tjpetrowski@mail.com', 1, 1, 'Images/NoPhoto.jpg', 1,''),
+		('T.J.', 'Petrowski', '1234', 24, 'Warren', 'Canada', 't.j.petrowski@robertsoncollege.net', 1, 1, 'Images/NoPhoto.jpg', 1,''),
 		('Chris', 'Jeffrey', '1234', 21, 'Winnipeg', 'Canada', 'chris.jeffrey@robertsoncollege.net', 1, 1, 'Images/NoPhoto.jpg', 1,''),
 		('Joseph', 'Maglalang', '1234', 30, 'Winnipeg', 'Canada', 'joseph.maglalang@robertsoncollege.net', 1, 1, 'Images/NoPhoto.jpg', 6,'')
 
